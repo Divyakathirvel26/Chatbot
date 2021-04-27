2e04b3852ee277a0ad274110c296ff6b55a2b195
Divyakathirvel26 <divyakathir398@gmail.com>
2021-04-27 20:30:21 +0530

    Initial Commit

---
+++ chatgui.py
@@ -0,0 +1,14 @@
+
+import nltk
+from nltk.stem import WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
+import pickle
+import numpy as np
+
+from keras.models import load_model
+model = load_model('chatbot_model.h5')
+import json
+import random
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl','rb'))
+classes = pickle.load(open('classes.pkl','rb'))
